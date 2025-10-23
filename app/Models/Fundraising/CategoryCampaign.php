a9488ae9aba10964c914b528f1b83043a40227f5
Yatim Mandiri <muhammadhamdani017@gmail.com>
2025-10-24 05:02:51 +0700

    Update CategoryCampaign.php

--- app/Models/Fundraising/CategoryCampaign.php
+++ app/Models/Fundraising/CategoryCampaign.php
@@ -3,8 +3,25 @@
 namespace App\Models\Fundraising;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CategoryCampaign extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'icon',
+        'featured_image',
+        'status',
+        'recommended',
+    ];
+
+    public function campaigns(): HasMany
+    {
+        return $this->hasMany(Campaign::class, 'category_id');
+    }
 }
